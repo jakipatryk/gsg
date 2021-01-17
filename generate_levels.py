512f2462cc4d9cbe26a749f4db1cc6b3b3f6bdc2
jakipatryk <18032838+jakipatryk@users.noreply.github.com>
2021-01-17 02:44:15 +0100

    Remove numpy

--- generate_levels.py
+++ generate_levels.py
@@ -1,29 +1,37 @@
-import numpy as np
 from math import sqrt
 from game import Level
+
+
+def to_2d(arr, board_size):
+    return [[arr[i * board_size + j] for j in range(board_size)] for i in range(board_size)]
+
 
 def generate_levels(board_size):
     levels = list()
 
-    #LEVEL 1
-    grid = np.array([i%2 for i in range(board_size**2)])
-    levels.append(Level(grid.reshape(board_size, board_size), np.sum(grid)+2, np.sum(grid)))
+    # LEVEL 1
+    grid = [i % 2 for i in range(board_size**2)]
+    levels.append(Level(to_2d(grid, board_size),
+                        sum(grid) + 2, sum(grid)))
 
     # LEVEL 2
-    grid = np.array([0 for i in range(board_size ** 2)])
+    grid = [0 for i in range(board_size ** 2)]
     for i in range(board_size):
         grid[i**2] = 1
-    levels.append(Level(grid.reshape(board_size, board_size), np.sum(grid)+2, np.sum(grid)))
+    levels.append(Level(to_2d(grid, board_size),
+                        sum(grid) + 2, sum(grid)))
 
     # LEVEL 3
-    grid = np.array([0 for i in range(board_size ** 2)])
-    primes = [x for x in range(2, board_size**2) if not [i for i in range(2, int(sqrt(x)) + 1) if x % i == 0]]
+    grid = [0 for i in range(board_size ** 2)]
+    primes = [x for x in range(
+        2, board_size**2) if not [i for i in range(2, int(sqrt(x)) + 1) if x % i == 0]]
     for prime in primes:
         grid[prime] = 1
-    levels.append(Level(grid.reshape(board_size, board_size), np.sum(grid)+2, np.sum(grid)))
+    levels.append(Level(to_2d(grid, board_size),
+                        sum(grid) + 2, sum(grid)))
 
     # LEVEL 4
-    grid = np.array([0 for i in range(board_size ** 2)])
+    grid = [0 for i in range(board_size ** 2)]
     fibs = [0, 1]
     n = 1
     while fibs[n-1] + fibs[n] < board_size ** 2:
@@ -31,10 +39,11 @@ def generate_levels(board_size):
         n += 1
     for fib in fibs:
         grid[fib] = 1
-    levels.append(Level(grid.reshape(board_size, board_size), np.sum(grid)+2, np.sum(grid)))
+    levels.append(Level(to_2d(grid, board_size),
+                        sum(grid) + 2, sum(grid)))
 
     # LEVEL 5
-    grid = np.array([0 for i in range(board_size ** 2)])
+    grid = [0 for i in range(board_size ** 2)]
     catalans = [1]
     n = 0
     while True:
@@ -47,7 +56,7 @@ def generate_levels(board_size):
         n += 1
     for catalan in catalans:
         grid[catalan] = 1
-    levels.append(Level(grid.reshape(board_size, board_size), np.sum(grid)+2, np.sum(grid)))
+    levels.append(Level(to_2d(grid, board_size),
+                        sum(grid) + 2, sum(grid)))
 
     return levels
-
